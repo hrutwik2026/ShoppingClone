3684ac7a26271e63c0283099cbecaf8265510451
kale hrutwik <kalehrutwik@gmail.com>
2024-06-18 15:21:00 +0530

    first commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,26 @@
+import { CartContext } from "../store/Shopping-CarT-Context";
+import { useContext, useState } from "react";
+export default function Product({
+  id,
+  image,
+  title,
+  price,
+  description,
+}) {
+    const {addItemsToCart}=useContext(CartContext);
+  return (
+    <article className="product">
+      <img src={image} alt={title} />
+      <div className="product-content">
+        <div>
+          <h3>{title}</h3>
+          <p className='product-price'>${price}</p>
+          <p>{description}</p>
+        </div>
+        <p className='product-actions'>
+          <button onClick={() => addItemsToCart(id)}>Add to Cart</button>
+        </p>
+      </div>
+    </article>
+  );
+}
